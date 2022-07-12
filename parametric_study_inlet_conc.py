2aa86a47943192b421af2712ea172dc7743045c4
J Dark <james.dark.95@gmail.com>
2022-07-12 20:04:40 +0000

    works with FESTIM 0.10.0

--- parametric_study_inlet_conc.py
+++ parametric_study_inlet_conc.py
@@ -1,35 +1,46 @@
-from parameters import parameters
+from parameters import my_model, inlet_h_concentration
 from solve_H_transport import run_H_transport
 import numpy as np
+import FESTIM as F
 
 
-if __name__ == "__main__":
+test_values = np.linspace(0, 1, num=11)
 
-    test_values = [np.linspace(0, 1, num=11)]
+folder = "Results/parametric_studies/varying_inlet_conc/"
+for eta in test_values:
+    results_folder = folder + "eta={:.1f}/".format(eta)
+    for export in my_model.exports.exports:
+        if isinstance(export, F.DerivedQuantities):
+            export.filename = results_folder + "derived_quantities.csv"
+        elif isinstance(export, F.XDMFExport):
+            export.folder = results_folder
+            export.append = False
+            export.define_xdmf_file()
 
-    folder = "parametric_testing_inlet_conc"
-    for eta in test_values:
-        parameters['exports']["xdmf"]["folder"] = folder + \
-            '/eta={:.1f}'.format(eta)
-        parameters['exports']["derived_quantities"]["folder"] = folder + \
-            '/eta={:.1f}'.format(eta)
-        print('Current step is eta = {:.1f}'.format(eta))
-        relative_error = 1  # initialise relative error
-        average_concentration_outlet_old = 1.01741917005023E+21/0.061  # initialise average concentration outlet old to smth
-        while relative_error > 0.01:
-            # run FESTIM
-            output = run_H_transport(parameters, log_level=20)
+    print("Current step is eta = {:.1f}".format(eta))
+    relative_error = 1  # initialise relative error
+    average_concentration_outlet_old = (
+        1.01741917005023e21 / 0.061
+    )  # initialise average concentration outlet old to smth
+    while relative_error > 0.01:
+        # run FESTIM
+        derived_quantities = run_H_transport(my_model)
 
-            # compute what the average conc at the outlet is
-            index = np.where(np.array(output["derived_quantities"][0]) == "Total solute surface 21")
-            average_concentration_outlet = output["derived_quantities"][1][index[0][0]]/0.061
+        # compute what the average conc at the outlet is
+        index = np.where(
+            np.array(derived_quantities.data[0]) == "Total solute surface 21"
+        )
+        average_concentration_outlet = derived_quantities.data[1][index[0][0]] / 0.061
 
-            # modify the BC accordingly
-            parameters["boundary_conditions"][0]["value"] = eta*average_concentration_outlet
+        # modify the BC accordingly
+        inlet_h_concentration.value = eta * average_concentration_outlet
 
-            # compute the relative error
-            relative_error = (average_concentration_outlet-average_concentration_outlet_old)/average_concentration_outlet_old
-            # store the old value of conc outlet
-            average_concentration_outlet_old = average_concentration_outlet
-            print('relative error = ', relative_error)
-            print('refining')
+        # compute the relative error
+        relative_error = (
+            average_concentration_outlet - average_concentration_outlet_old
+        ) / average_concentration_outlet_old
+
+        # store the old value of conc outlet
+        average_concentration_outlet_old = average_concentration_outlet
+        print("relative error = ", relative_error)
+        print("refining")
